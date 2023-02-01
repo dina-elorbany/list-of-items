096393842a8424bd5b12cb1ea4153bdb85f786bb
Dina-Abdou-Elorbany <dina.elorbany@gmail.com>
2023-02-01 17:55:51 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
-function App() {
+/** 
+  TODO: when user clicks the Add new Item button a new input should appear
+  TODO: added inputs should not exceed maxNumber
+  TODO: when user clicks Submit button all inserted input values should appear on the list
+  */
+
+import { useState } from 'react';
+
+const App = () => {
+  const maxNumber = 10;
+
+  const [inputs, setInputs] = useState(['']);
+  const [resutls, setResutls] = useState([]);
+
+  const addFields = () => {
+    inputs.length < maxNumber && setInputs(pre => [...pre, '']);
+  };
+
+  const handleChange = (e, index) => {
+    const values = inputs.map((input, i) => {
+      if (index === i) {
+        return e.target.value;
+      } else {
+        return input;
+      }
+    });
+    setInputs(values);
+  };
+
+  const submit = () => {
+    setResutls([...inputs]);
+    console.log(inputs);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='body'>
+      <div className='list-wrapper'>
+        <h2> Max List Number {maxNumber} </h2>
+
+        <form onSubmit={submit} className='form'>
+          {inputs.map((input, index) => (
+            <input
+              value={input}
+              name='value'
+              placeholder=''
+              key={index}
+              onChange={e => handleChange(e, index)}
+            />
+          ))}
+        </form>
+
+        <div className='buttons-wrappers'>
+          <button onClick={() => addFields()}>Add new Item</button>
+          <button onClick={submit}>submit</button>
+        </div>
+      </div>
+      <div className='list-result'>
+        <h2> List Result </h2>
+        <ul>
+          {resutls.map((res, i) => (
+            <li key={i}>{res}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
